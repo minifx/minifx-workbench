c611611a2a89a70d3e05ed30615a62d018a11979
kaifox <kajetan.fuchsberger@cern.ch>
2019-01-25 14:40:49 +0100

    fixing pom to be compatible with maven central

--- scripts/prepare-artifacts.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-#
-# this script assumes to be run from the project dir.
-
-mkdir projectpage
-cp -rv build/reports/* projectpage
-cp -rv build/docs/* projectpage
-cp -rv docs/* projectpage
-
